d8d156201fbe8df442ab6dffea7713be114862e5
Onkar Ganjewar <ganjewaronkar@gmail.com>
2018-06-22 23:51:40 -0700

    Fix typo

--- models/kaggle_cats_dogs_caffenet/resume_training.sh
+++ models/kaggle_cats_dogs_caffenet/resume_training.sh
@@ -7,7 +7,7 @@ CAFFE_ROOT_DIR="/Users/ganon01/Documents/Work/Hackathon/image-classification/caf
 
 $CAFFE_ROOT_DIR/build/tools/caffe train \
     --solver=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/solver.prototxt \
-    --snapshot=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffenet_train_iter_1041.solverstate >> $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train.log 2>&1 \
+    --snapshot=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train_iter_1041.solverstate >> $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train.log 2>&1 \
     | tee $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train.log &
 
 echo "Training resumed..."

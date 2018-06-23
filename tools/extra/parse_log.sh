0c773fe606a1078f8a7270cc4fd99a213b7df997
Onkar Ganjewar <ganjewaronkar@gmail.com>
2018-06-23 14:32:50 -0700

    Make parse_log.sh work!!

--- tools/extra/parse_log.sh
+++ tools/extra/parse_log.sh
@@ -4,6 +4,7 @@
 #     caffe.log.test (columns: '#Iters Seconds TestAccuracy TestLoss')
 #     caffe.log.train (columns: '#Iters Seconds TrainingLoss LearningRate')
 
+set -ex
 
 # get the dirname of the script
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -13,13 +14,14 @@ then
 echo "Usage parse_log.sh /path/to/your.log"
 exit
 fi
+
 LOG=`basename $1`
 sed -n '/Iteration .* Testing net/,/Iteration *. loss/p' $1 > aux.txt
-sed -i '/Waiting for data/d' aux.txt
-sed -i '/prefetch queue empty/d' aux.txt
-sed -i '/Iteration .* loss/d' aux.txt
-sed -i '/Iteration .* lr/d' aux.txt
-sed -i '/Train net/d' aux.txt
+sed -i .bak '/Waiting for data/d' aux.txt
+sed -i .bak '/prefetch queue empty/d' aux.txt
+sed -i .bak '/Iteration .* loss/d' aux.txt
+sed -i .bak '/Iteration .* lr/d' aux.txt
+sed -i .bak '/Train net/d' aux.txt
 grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
 grep 'Test net output #0' aux.txt | awk '{print $11}' > aux1.txt
 grep 'Test net output #1' aux.txt | awk '{print $11}' > aux2.txt
@@ -48,4 +50,4 @@ $DIR/extract_seconds.py aux.txt aux3.txt
 # Generating
 echo '#Iters Seconds TrainingLoss LearningRate'> $LOG.train
 paste aux0.txt aux3.txt aux1.txt aux2.txt | column -t >> $LOG.train
-rm aux.txt aux0.txt aux1.txt aux2.txt  aux3.txt
+rm aux.txt aux0.txt aux1.txt aux2.txt aux3.txt

c9fdc6d04531676f1b8dacb5718103c55edd3c28
Onkar Ganjewar <ganjewaronkar@gmail.com>
2018-06-22 23:43:23 -0700

    Refactor scripts and use full paths

--- models/kaggle_cats_dogs_alexnet/start_train_net.sh
+++ models/kaggle_cats_dogs_alexnet/start_train_net.sh
@@ -3,8 +3,10 @@ set -ex
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-CAFFE_ROOT_DIR=$DIR/../../../caffe
+CAFFE_ROOT_DIR="/Users/ganon01/Documents/Work/Hackathon/image-classification/caffe"
 
 $CAFFE_ROOT_DIR/build/tools/caffe train \
-    --solver=$CAFFE_ROOT_DIR/models/bvlc_googlenet/solver.prototxt >> $DIR/bvlc_googlenet_train.log 2>&1 \
-    | tee $DIR/bvlc_googlenet_train.log $@
+    --solver=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_alexnet/solver.prototxt >> $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_alexnet/caffe_alexnet_train.log 2>&1 \
+    | tee $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_alexnet/caffe_alexnet_train.log &
+
+echo "Training started..."

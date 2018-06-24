77172b72fd1efc40692d9e8405d3f1ca02739a8b
Onkar Ganjewar <ganjewaronkar@gmail.com>
2018-06-24 12:40:30 -0700

    Add weights for kaggle_cats_dogs_caffenet in start_train_net.sh

--- models/kaggle_cats_dogs_caffenet/start_train_net.sh
+++ models/kaggle_cats_dogs_caffenet/start_train_net.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -ex
 
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+# DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 CAFFE_ROOT_DIR="/Users/ganon01/Documents/Work/Hackathon/image-classification/caffe"
 
 $CAFFE_ROOT_DIR/build/tools/caffe train \
-    --solver=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/solver.prototxt >> $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train.log 2>&1 \
-    | tee $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_train.log &
+    --solver=$CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/solver.prototxt --weights $CAFFE_ROOT_DIR/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel >> $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_transfer_train.log 2>&1 \
+    | tee $CAFFE_ROOT_DIR/models/kaggle_cats_dogs_caffenet/caffe_caffenet_transfer_train.log &
 
 echo "Training started..."
